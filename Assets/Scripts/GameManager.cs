7b839d176dfecb8d70096ae9841abc4a57648673
AdrianSardina <112350190+AdrianSardina@users.noreply.github.com>
2025-11-02 14:45:02 -0300

    animacion del cazador

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int animalCount;
+    float elapsedTime;
+    int timeLimit = 300; // 5 minutos
     void Start()
     {
-        
+        elapsedTime = 0f;
+        animalCount = GameObject.FindGameObjectsWithTag("Animal").Length;
+        StartCoroutine(StartTime());
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+      //  Debug.Log("Elapsed Time: " + elapsedTime);
+        if(elapsedTime >= timeLimit)
+        {
+            Debug.Log("Victory! You have protected the animals.");
+            //ir a pantalla de victoria
+        }
+
+    }
+    IEnumerator StartTime() 
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            elapsedTime += 1;
+        }
+    }
+    public void TriggerDefeat()
+    {
+        Debug.Log("Defeat! An animal was caught by a hunter.");
+        //ir a pantalla de derrota
     }
 }
